9ded8ebc125cceb5b8549db475401f5518acf76b
maueli <maueli89@gmail.com>
2021-12-15 20:48:30 -0300

    Template Navigation + Redux + Persist

--- store/infoSlice.js
+++ store/infoSlice.js
@@ -1,57 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    uid:[],
-    mail:[],
-    pass:[],
-    name:[],
-    isLogged:false,
-    isProductor:false,
-    coordinates:[],
-    disponible:false,
-    stopLoc:[]
+    listAlbums : [],
+    listPhotos : []
 }
 
 
-const Log = createSlice({
-    name: 'log',
+const Info = createSlice({
+    name: 'info',
     initialState,
     reducers: {
-        logUid:(state,action)=>{
-          state.uid = action.payload;
+        addListAlbums:(state,action)=>{
+            state.listAlbums = action.payload;
         },
-        logData:(state,action)=>{
-          //console.log("LogSlice:" + action.payload.mail)
-          state.mail = action.payload.mail;
-          state.pass = action.payload.pass;
-        },
-        logDataName:(state,action)=>{
-            state.name = action.payload;
-        },
-        logStatus:(state,action)=>{
+        addListPhotos:(state,action)=>{
             return {
                 ...state,
                 isLogged: action.payload
             }
         },
-        dispoCoordinates:(state,action)=>{
-            return {
-                ...state,
-                coordinates: action.payload
-            }
-        },
-        disponibilidad:(state,action)=>{
-            return {
-                ...state,
-                disponible: action.payload
-            }
-        },
-        isProductor:(state,action)=>{
-            state.isProductor = action.payload;
-        },
-        StopLoc:(state,action)=>{
-            state.stopLoc = action.payload;
-        }
         /*addAllImages:(state,action)=>{
           state.allImages = [...state.allImages, action.payload]
         },*/
@@ -59,6 +26,6 @@ const Log = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { logUid, logData, logDataName, logStatus, dispoCoordinates, disponibilidad, isProductor, StopLoc } = Log.actions
+export const {addListAlbums, addListPhotos  } = Info.actions
 
-export default Log.reducer
+export default Info.reducer
