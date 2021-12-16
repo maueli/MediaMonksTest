dac485ac88ec9587ea69fac72b187e16b7effe3f
maueli <maueli89@gmail.com>
2021-12-16 12:04:05 -0300

    Styling lists

--- components/Albums.js
+++ components/Albums.js
@@ -5,8 +5,9 @@ const  Albums=()=>{
     const styles = {
         container:{
             flex:1,
+            alignItems:"center",
             justifyContent:"center",
-            alignItems:"center"
+            width:"100%"
         }
     }
 
@@ -19,15 +20,39 @@ const  Albums=()=>{
     },[]);
 
     const renderItem = ({item})=>{
+        const styles2 = {
+            container:{
+                justifyContent:"center",
+                alignItems:"center",
+                padding:20,
+            },
+            container2:{
+                width:"80%",
+                height:120,
+                borderRadius:10,
+                justifyContent:"center",
+                alignItems:"center",
+                backgroundColor:"gray",
+                padding:20
+            },
+            text:{
+                color:"white"
+            }
+        }
         return(
-            <Text style={{height:200}}> {item.title} </Text>
+            <View style={styles2.container}>
+                <View style={styles2.container2}>
+                    <Text style={styles2.text} > {item.title} </Text>
+                </View>
+            </View>
         )
     }
 
-
+// contentContainerStyle={{ alignItems:"center"}}
     return(
         <View style={styles.container}>
             <FlatList
+                style={{width:"100%"}}
                 data={albums}
                 renderItem={renderItem}
                 keyExtractor={(item)=>item.id} 
